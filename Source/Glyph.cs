60dc12ee8e4f01acb3bc60538588ffa0917b7660
Th4tGuyyy <carson-dickinson@hotmail.com>
2020-02-11 21:58:54 -0600

    Added glyph class, and added pixelat function

---
+++ Source/Glyph.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleGameEngine
+{
+	public class Glyph
+	{
+		public char c;
+		public int fg;
+		public int bg;
+
+		public void set(char c_, int fg_, int bg_) { c = c_; fg = fg_;bg = bg_; }
+
+		public void clear() { c = (char)0;fg = 0;bg = 0; }
+	}
+}
